d4df5c520c63b3b56e203b2013b48b8cc28df05c
dyussenov <dyusenov.aset.arm@gmail.com>
2024-05-10 03:42:23 +0500

    .

--- script.js
+++
@@ -1,6 +0,0 @@
-let WebApp = window.Telegram.WebApp;
-
-WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
-
-let username = document.getElementById("username")
-username.innerHTML = `${WebApp.WebAppUser.username}`
